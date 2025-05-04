84d7a0e4d34d9ecc24e068d4bfea9c939bed166c
MezaRE <rodrigo.meza@usil.pe>
2025-05-03 19:14:51 -0500

    Actualización_Meza_2

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,36 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-in main(void){
+int main(void){
   DDRD |=0xC0; 
   PORTD &=~(0xC0);
   DDRD &=~(0x20);
   PORTD |=0x20
 
-  char flag=0;
+  char flag = 0;
 
   while(1){
-    if(!(PIND & 0x20))){
+    if(!(PIND & 0x20)){
       _delay_ms(100);
-      switch(flag);
       switch(flag){
         caso 0:
         PORD |=(0X80)
         flag = 1;
         break;
 
-    
+        case 1:
+        PORTD &= ~(0xC0);
+        _delay_ms(50);
+        PORTD |= (0x40);
+
+        flag = 2
+        break;
+        
+        case 2:
+        PORTD &=~(0xC0);
+        flag= 0;
+        break;
+      
       }
     }
   }
